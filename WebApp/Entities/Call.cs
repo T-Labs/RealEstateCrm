ebd7d1b7654419e07d4a5faa2b38202440969596
Artur Teregulov <sliceice@gmail.com>
2016-03-10 21:49:54 +0300

    Update Call.cs

--- WebApp/Entities/Call.cs
+++ WebApp/Entities/Call.cs
@@ -10,9 +10,13 @@ namespace WebApp.Entities
     {
         public int Id { get; set; }
 
-        public string Name { get; set; }
+        public string Status { get; set; }
 
         public DateTime  Date { get; set; }
+        
+        public int HousingId { get; set; }
+        
+        public Housing Housing { get; set; }
 
         public string ApplicationUserId { get; set; }
 
